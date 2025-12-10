92dcc76bc39b2b904a1f538f8c9ffc15bcbae311
daffaaptar <daffaaptapratama@gmail.com>
2025-12-10 16:53:36 +0700

    [CRUD] Add Entity Mahasiswa & CRUD Function

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { MahasiswaModule } from './mahasiswa/mahasiswa.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, MahasiswaModule],
   controllers: [AppController],
   providers: [AppService],
 })
